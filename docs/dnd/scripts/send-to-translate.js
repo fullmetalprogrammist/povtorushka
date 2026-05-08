90ac6e66064e5e5adf18d505843e5dd994b0862f
fmp <come.as@you.are>
2026-05-08 17:13:36 +0300

    Исправил баг скролла из-за удержания пальца на реплике на мобиле

--- docs/dnd/scripts/send-to-translate.js
+++ docs/dnd/scripts/send-to-translate.js
@@ -1,17 +1,24 @@
 document.querySelectorAll('p.line').forEach(el => {
     let timer;
+    let touchStarted = false;
     const start = (e) => {
-        if (e.cancelable) e.preventDefault();
+        touchStarted = true;
         timer = setTimeout(() => {
-            const text = encodeURIComponent(el.textContent);
-            window.open(`https://translate.google.com/?sl=en&tl=ru&text=${text}&op=translate`);
+            if (touchStarted) {
+                if (e.cancelable) e.preventDefault();
+                const text = encodeURIComponent(el.textContent);
+                window.open(`https://translate.google.com/?sl=en&tl=ru&text=${text}&op=translate`);
+            }
         }, 1000);
     };
-    const cancel = () => clearTimeout(timer);
+    const cancel = () => {
+        touchStarted = false;
+        clearTimeout(timer);
+    };
     el.addEventListener('mousedown', start);
     el.addEventListener('mouseup', cancel);
     el.addEventListener('mouseleave', cancel);
-    el.addEventListener('touchstart', start, {passive: false});
+    el.addEventListener('touchstart', start, {passive: true});
     el.addEventListener('touchend', cancel);
     el.addEventListener('touchcancel', cancel);
 });
